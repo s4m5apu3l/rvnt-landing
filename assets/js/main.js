e1134a79347d6a809240dca32d1f71cd45b23b31
s4m5apu3l <asperatuz@yandex.ru>
2024-02-26 12:16:09 +0700

    init: add project

---
+++ assets/js/main.js
@@ -0,0 +1,191 @@
+import { CountUp } from './countUp.min.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.dir('init js');
+    const burgerBtn = document.querySelector('.l-header__burger');
+    const headerNav = document.querySelector('.l-header__nav');
+
+    burgerBtn.addEventListener('click', () => {
+        headerNav.classList.toggle('active');
+        burgerBtn.classList.toggle('active');
+    });
+
+    const swiperCases = new Swiper('.l-cases__swiper', {
+        // Optional parameters
+        // loop: true,
+        // initialSlide: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+                grid: {
+                    rows: 2,
+                    fill: 'row',
+                },
+                slidesPerView: 1.1,
+                centeredSlides: true,
+            },
+            390: {
+                grid: {
+                    rows: 2,
+                    fill: 'row',
+                },
+                slidesPerView: 1.2,
+                centeredSlides: false,
+            },
+            724: {
+                grid: {
+                    rows: 1,
+                },
+                slidesPerView: 1.2,
+            },
+            750: {
+                slidesPerView: 2,
+            },
+            1300: {
+                slidesPerView: 3,
+            },
+        },
+        navigation: {
+            nextEl: '.custom-swiper-btn-next',
+            prevEl: '.custom-swiper-btn-prev',
+        },
+    });
+    const swiperServices = new Swiper('.l-services__swiper', {
+        spaceBetween: 20,
+        // centeredSlides: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1.1,
+            },
+            724: {
+                slidesPerView: 1.2,
+            },
+            1024: {
+                slidesPerView: 2,
+            },
+        },
+    });
+    const swiperProjects = new Swiper('.l-projects__swiper', {
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            640: {
+                spaceBetween: 20,
+                slidesPerView: 1.6,
+            },
+            924: {
+                slidesPerView: 2,
+            },
+            1420: {
+                slidesPerView: 3,
+            },
+        },
+        navigation: {
+            nextEl: '.l-projects--swiper-btn-next',
+            prevEl: '.l-projects--swiper-btn-prev',
+        },
+    });
+
+    const swiperCertificate = new Swiper('.l-certificate__swiper', {
+        effect: 'creative',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+
+        breakpoints: {
+            0: {
+                creativeEffect: {
+                    enabled: false,
+                    prev: {
+                        // scale: 1,
+                        translate: ['-110%', 0, 0],
+                    },
+                    next: {
+                        // scale: 1,
+                        translate: ['110%', 0, 0],
+                    },
+                    limitProgress: 2,
+                },
+            },
+            590: {
+                creativeEffect: {
+                    prev: {
+                        scale: 0.7,
+                        translate: ['-100%', 0, 0],
+                    },
+                    next: {
+                        scale: 0.7,
+                        translate: ['100%', 0, 0],
+                    },
+                    limitProgress: 2,
+                },
+            },
+        },
+        navigation: {
+            nextEl: '.l-certificate--swiper-btn-next',
+            prevEl: '.l-certificate--swiper-btn-prev',
+        },
+    });
+
+    // для формата чисел
+    const formatNumber = number => {
+        return number.toLocaleString('ru-RU') + ' ₽';
+    };
+
+    const elements = document.querySelectorAll('.l-number');
+
+    elements.forEach(element => {
+        const number = parseInt(element.textContent.replace(/\s+/g, ''), 10); // Удаление пробелов и преобразование в число
+        if (!isNaN(number)) {
+            element.textContent = formatNumber(number);
+        }
+    });
+
+    //для табов
+    const tabsBtn = document.querySelectorAll('.l-cases__tab-btn');
+
+    tabsBtn.forEach(tab => {
+        tab.addEventListener('click', () => {
+            document
+                .querySelectorAll('.l-cases__tab-btn.active')
+                .forEach(activeTab => activeTab.classList.remove('active'));
+            document
+                .querySelectorAll('.l-tab-content.active')
+                .forEach(activeContent => activeContent.classList.remove('active'));
+
+            tab.classList.add('active');
+
+            const targetContent = document.querySelector(tab.dataset.tabBtn);
+            targetContent.classList.add('active');
+        });
+    });
+
+    const countNums = () => {
+        const numbersObserver = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const count = new CountUp(
+                        entry.target.id, // 1. задаём идентификатор элемента с числом
+                        entry.target.dataset.num, // 3. задаём конечное число (берем из data-атрибута)
+                        entry.target.dataset.duration || 4, // 5. задаём продолжительность анимации в секундах (если у элемента есть атрибут data-duration, то берём из него значение, иначе назначаем 4 секунды по-умолчанию)
+                        {
+                            useEasing: true, //анимация
+                            separator: ' ', // задаём разделитель групп разрядов (например для миллиона - 1 000 000)
+                            prefix: entry.target.dataset.prefix || '', // задаём префикс - любые символы перед числом (берем значение из data-prefix, если не указано - то задаем пустую строку по умолчанию)
+                            suffix: entry.target.dataset.suffix || '', // задаём суффикс - любые символы после числа (берем значение из data-suffix, если не указано - то задаем пустую строку по умолчанию)
+                        }
+                    );
+                    count.start();
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+        document.querySelectorAll('.js-count').forEach(num => {
+            numbersObserver.observe(num);
+        });
+    };
+    countNums();
+});
